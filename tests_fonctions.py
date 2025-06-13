6f7f7ab9979d3a8521c6357cbec998c340e9fa37
Perrine Launay <plaunay@medinplus.com>
2025-06-13 10:44:55 +0200

    TU pair/impair

--- tests_fonctions.py
+++ tests_fonctions.py
@@ -15,3 +15,12 @@ class TestFonctions(unittest.TestCase):
     def test_addition_cas_zero(self):
         resultat = additionner(0, 0)
         self.assertEqual(resultat, 0)    
+
+    def test_est_pair_cas_pair(self):
+        self.assertTrue(est_pair(4))
+
+    def test_est_pair_cas_impair(self):
+        self.assertFalse(est_pair(5))
+
+    def test_est_pair_cas_zero(self):
+        self.assertTrue(est_pair(0))
